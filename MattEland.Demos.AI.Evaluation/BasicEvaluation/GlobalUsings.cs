d0064c14ca56a8aa5e6d7f29e8ef392311900359
Matt Eland <matt.eland@leadingedje.com>
2025-05-27 14:00:33 -0400

    A little more organization

---
+++ MattEland.Demos.AI.Evaluation/BasicEvaluation/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using System.ClientModel;
+global using Microsoft.Extensions.AI;
+global using Microsoft.Extensions.AI.Evaluation;
+global using Microsoft.Extensions.AI.Evaluation.Quality;
+global using Microsoft.Extensions.Configuration;
+global using OpenAI;
+global using Spectre.Console;
+global using ChatMessage = Microsoft.Extensions.AI.ChatMessage;
