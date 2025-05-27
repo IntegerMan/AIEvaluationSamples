7a616cf7a6cb4adaad0756fc16b3d5c7fd0457dd
Matt Eland <matt.eland@leadingedje.com>
2025-05-27 10:29:41 -0400

    Added boilerplate and references

---
+++ MattEland.Demos.AI.Evaluation/BasicEvaluation/EvaluationDemoSettings.cs
@@ -0,0 +1,7 @@
+﻿public class EvaluationDemoSettings
+{
+	public required string OpenAIEndpoint { get; init; }
+	public required string OpenAIKey { get; init; }
+	public required string TextModelName { get; init; }
+	public required string EmbeddingModelName { get; init; }
+}
---
+++ MattEland.Demos.AI.Evaluation/BasicEvaluation/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Spectre.Console;
+
+// Everything is better with a nice header
+IAnsiConsole console = AnsiConsole.Console;
+console.Write(new FigletText("Kernel Memory").Color(Color.Yellow));
+console.MarkupLine("[cyan]Kernel Memory Document Search Demo[/]");
+console.WriteLine();
+
+// Load Settings
+IConfiguration config = new ConfigurationBuilder()
+	.AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+	.AddEnvironmentVariables()
+	.AddUserSecrets<Program>()
+	.AddCommandLine(args)
+	.Build();
+EvaluationDemoSettings settings = config.Get<EvaluationDemoSettings>()!;
